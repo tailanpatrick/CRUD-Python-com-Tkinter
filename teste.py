68dc71725196deade175b17449678389450287aa
tailanpatrick <tailanpatrick06@gmail.com>
2024-01-23 14:22:41 -0300

    deletado teste.py usado durante desenvolvimento

--- teste.py
+++
@@ -1,9 +0,0 @@
-from model.produtoDAO import ProdutoDAO
-from model.produto import Produto
-
-produtoDao = ProdutoDAO()
-produto = Produto("Teste", 4.50)
-
-produtoDao.cadastrar_produto(produto)
-resultado = produtoDao.listar_produtos()
-print(resultado)
